33d4f483242372fcf5acea9adb3e5fe05b3771e1
romanirofaeil <110042363+romanirofaeil@users.noreply.github.com>
2024-04-10 12:40:10 +0200

    Update alu_monitor.sv

--- dv/env/alu_monitor.sv
+++ dv/env/alu_monitor.sv
@@ -7,53 +7,48 @@ class alu_monitor extends uvm_monitor;
     function new(string name = "alu_monitor", uvm_component parent = null);
         super.new(name, parent);
     endfunction
-    extern virtual function void build_phase(uvm_phase phase);
-    extern virtual task run_phase(uvm_phase phase);
-    extern virtual task input_monitoring();
-    extern virtual task output_monitoring();
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        in_mon_analysis_port = new("in_mon_analysis_port", this);
+        out_mon_analysis_port = new("out_mon_analysis_port", this);
+    endfunction
+    virtual task run_phase(uvm_phase phase);
+        super.run_phase(phase);
+        fork
+            input_monitoring();
+            output_monitoring();
+        join
+    endtask
+    virtual task input_monitoring();
+        alu_sequence_item alu_seq_item;
+        forever begin
+            @(posedge alu_vif.clk)
+                if(alu_vif.ALU_en) begin
+                    alu_seq_item = alu_sequence_item::type_id::create("alu_seq_item");
+                    alu_seq_item.ALU_en = alu_vif.ALU_en;
+                    alu_seq_item.A = alu_vif.A;
+                    alu_seq_item.B = alu_vif.B;
+                    alu_seq_item.a_en = alu_vif.a_en;
+                    alu_seq_item.a_op = alu_vif.a_op;
+                    alu_seq_item.b_en = alu_vif.b_en;
+                    alu_seq_item.b_op = alu_vif.b_op;
+                    in_mon_analysis_port.write(alu_seq_item);
+                    alu_seq_items.push_back(alu_seq_item);
+                end
+        end
+    endtask
+    virtual task output_monitoring();
+        alu_sequence_item alu_seq_item;
+        forever begin
+            @(posedge alu_vif.clk)
+                if(alu_vif.C_en) begin
+                    if(alu_seq_items.size() == 0)
+                        `uvm_fatal(get_type_name(), "Received Output Item Without Sending Any Input Items!")
+                    alu_seq_item = alu_seq_items.pop_front();
+                    alu_seq_item.C_en = alu_vif.C_en;
+                    alu_seq_item.C = alu_vif.C;
+                    out_mon_analysis_port.write(alu_seq_item);
+                end
+        end
+    endtask
 endclass
-
-virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    in_mon_analysis_port = new("in_mon_analysis_port", this);
-    out_mon_analysis_port = new("out_mon_analysis_port", this);
-endfunction
-virtual task run_phase(uvm_phase phase);
-    super.run_phase(phase);
-    fork
-        input_monitoring();
-        output_monitoring();
-    join
-endtask
-virtual task input_monitoring();
-    alu_sequence_item alu_seq_item;
-    forever begin
-        @(posedge alu_vif.clk)
-            if(alu_vif.ALU_en) begin
-                alu_seq_item = alu_sequence_item::type_id::create("alu_seq_item");
-                alu_seq_item.ALU_en = alu_vif.ALU_en;
-                alu_seq_item.A = alu_vif.A;
-                alu_seq_item.B = alu_vif.B;
-                alu_seq_item.a_en = alu_vif.a_en;
-                alu_seq_item.a_op = alu_vif.a_op;
-                alu_seq_item.b_en = alu_vif.b_en;
-                alu_seq_item.b_op = alu_vif.b_op;
-                in_mon_analysis_port.write(alu_seq_item);
-                alu_seq_items.push_back(alu_seq_item);
-            end
-    end
-endtask
-virtual task output_monitoring();
-    alu_sequence_item alu_seq_item;
-    forever begin
-        @(posedge alu_vif.clk)
-            if(alu_vif.C_en) begin
-                if(alu_seq_items.size() == 0)
-                    `uvm_fatal(get_type_name(), "Received Output Item Without Sending Any Input Items!")
-                alu_seq_item = alu_seq_items.pop_front();
-                alu_seq_item.C_en = alu_vif.C_en;
-                alu_seq_item.C = alu_vif.C;
-                out_mon_analysis_port.write(alu_seq_item);
-            end
-    end
-endtask
