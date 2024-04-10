204d4ec3975c5bfd60cc0f0f6f8306db2b95f8c4
romanirofaeil <110042363+romanirofaeil@users.noreply.github.com>
2024-04-10 12:37:29 +0200

    Update run.bash

--- dv/sim/run.bash
+++ dv/sim/run.bash
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 vcs -sverilog -ntb_opts uvm-1.2 ./alu_design.sv ./alu_testbench.sv
-./simv +UVM_TIMEOUT=5000000 +UVM_TESTNAME=alu_test +UVM_VERBOSITY=UVM_HIGH
+./simv +UVM_TIMEOUT=5000000 +UVM_TESTNAME=alu_test +UVM_VERBOSITY=UVM_LOW
 urg -lca -dir simv.vdb -report full_report -format text
 cd full_report
 cat *.txt
